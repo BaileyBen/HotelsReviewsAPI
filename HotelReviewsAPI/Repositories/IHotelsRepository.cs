72ac173a5666b338f3fc2aa326960d9e55326f97
BaileyBen <l3enl3ailey@hotmail.com>
2023-09-19 22:30:57 +1000

    /

--- HotelReviewsAPI/Repositories/IHotelsRepository.cs
+++ HotelReviewsAPI/Repositories/IHotelsRepository.cs
@@ -6,6 +6,7 @@ namespace HotelReviewsAPI.Repositories
     {
         Task<List<Hotel>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
             string? sortBy = null, bool? isAscending = true);
+
         Task<Hotel> GetByIdAsync(Guid id);
         Task<Hotel> CreateAsync(Hotel hotel);
         Task<Hotel> UpdateAsync(Guid id, Hotel hotel);
